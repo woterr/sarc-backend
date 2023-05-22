cd17c3d3a9b4afbc0eea3f4dc0cbc903576453d4
Glitch (entertaining-lovely-sesame) <none>
2023-05-22 18:06:38 +0000

    🐆📊 Checkpoint ./package.json:58374520/559 ./project.js:58374520/26

--- project.js
+++ project.js
@@ -1,7 +1,6 @@
 const projectModel = require("./Schemas/project.js");
 
 async function projectUpload(data) {
-  //   console.log(data);
   await projectModel
     .create({
       title: data.title,
